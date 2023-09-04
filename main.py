5eba46dc662381626a57ab16400a1753f8e170a6
SmartSlyFox <130297032+SmartSlyFox@users.noreply.github.com>
2023-09-04 13:28:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import asyncio
+from asyncio import sleep
 from time import time
 from web3 import AsyncWeb3
 from async_client import Client
 from data.config import WALLETS, rpc
 
+print("=================================================================================================")
+
 
 async def out(private_key, number):
-    client = Client(private_key=private_key, rpc=rpc)
-    print(f"Wallet {number}")
-    print(f"Connecting to network {await client.w3.eth.chain_id} : {await client.w3.is_connected()}")
+    while True:
+        client = Client(private_key=private_key, rpc=rpc)
+        chain_id = await client.w3.eth.chain_id
+        connected = await client.w3.is_connected()
+        balance = await client.w3.eth.get_balance(client.address)  # balance in wei
+        ether_balance = AsyncWeb3.from_wei(balance, 'ether')  # converting in ether
 
-    # balance in wei
-    balance = await client.w3.eth.get_balance(client.address)
-
-    # converting in ether
-    ether_balance = AsyncWeb3.from_wei(balance, 'ether')
-    print(f'Balance in ETH {ether_balance}')
-    print("=================================================================================================")
-
+        print(f"Wallet {number}")
+        print(f"Connecting to network {chain_id} : {connected}")
+        print(f'Balance in ETH {ether_balance}')
+        print("=================================================================================================")
+        # await sleep(1)
 
 async def main():
     tasks = []
@@ -28,8 +31,7 @@ async def main():
         task = asyncio.create_task(out(private_key, number))
         tasks.append(task)
 
-        await asyncio.gather(*tasks)
-
+    await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
     time_start = time()
